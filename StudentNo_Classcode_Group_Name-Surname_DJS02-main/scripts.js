59aedafb43f962a014fb111c6a89a91924c4ea65
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-05-08 20:20:41 +0200

    check for numeric errors

--- StudentNo_Classcode_Group_Name-Surname_DJS02-main/scripts.js
+++ StudentNo_Classcode_Group_Name-Surname_DJS02-main/scripts.js
@@ -18,14 +18,24 @@ if(!dividend || !divider ) {
 const validInputRegex = /^[0-9]+$/;
 if (!validInputRegex.test(dividend) || !validInputRegex.test(divider)) {
   result.classList.add("critical error");
-  result.innerText("Something critical went wrong, Please reload the page.");
+  result.innerText = "Something critical went wrong, Please reload the page.";
   return;
 }
 try {
   //Force Number conversion
   const dividendNumber = Number(dividend);
   const divisorNumber = Number(divider);
-  
+
+  //Check for other numeric errors
+  if (isNaN(dividendNumber) || isNaN(divisorNumber) || divisorNumber === 0) {
+    throw new error("Invalid input: Division by zero or non-numeric value procvided.");
+  }
+  const resultValue = Math.floor(dividendNumber / divisorNumber);
+  result.innerText = resultValue.toString();
+
+} catch (error) {
+  console.error("An error occured:", error);
+  console.error("call stack:", error.stack);
 }
 
 });
