ef9d780b8cf2350a762810f7394e9f52934272ff
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-05-08 17:43:55 +0200

    error handling, check if inputs are empty

--- StudentNo_Classcode_Group_Name-Surname_DJS02-main/scripts.js
+++ StudentNo_Classcode_Group_Name-Surname_DJS02-main/scripts.js
@@ -5,5 +5,16 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+ result.innerText = dividend / divider;
+
+//Error handling: check if input are empty
+if(!dividend || !divider ) {
+  result.classList.add("error-message")
+  result.innerText = "Division not performed. Both are required in inputs. Try again.";
+  return;
+}
+
 });
+
+
+
